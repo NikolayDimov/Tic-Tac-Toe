a44e470d883342bdfc19998b84bbc0dc26afdac8
Nikolay Dimov <114500017+NikolayDimov@users.noreply.github.com>
2024-08-19 00:09:56 +0300

    add gameboard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard.jsx";
 
 function App() {
 
-
-  return <main>
-    <div id="game-container">
-      <ol id="players">
-        <Player initialName="Player 1" symbol="X" />
-        <Player initialName="Player 2" symbol="O" />
-      </ol>
-      GAME BOARD
-    </div>
-
-    LOG
-  </main>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Player 1" symbol="X" />
+          <Player initialName="Player 2" symbol="O" />
+        </ol>
+        <GameBoard />
+      </div>
+      LOG
+    </main>
+  );
 }
 
 export default App;
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,33 +1,33 @@
 import { useState } from "react";
 
 export default function Player({ initialName, symbol }) {
-    const [playerName, setPlayerName] = useState(initialName);
-    const [isEditing, setIsEditing] = useState(false);
+	const [playerName, setPlayerName] = useState(initialName);
+	const [isEditing, setIsEditing] = useState(false);
 
-    function handleEditClick() {
-        setIsEditing((editing) => !editing);
-    }
+	function handleEditClick() {
+		setIsEditing((editing) => !editing);
+	}
 
-    function handleChange(event) {
-        setPlayerName(event.target.value);
-    }
+	function handleChange(event) {
+		setPlayerName(event.target.value);
+	}
 
-    let editablePlayerName = <span className="player-name">{playerName}</span>;
-    // let btnCaption = 'Edit';
+	let editablePlayerName = <span className="player-name">{playerName}</span>;
+	// let btnCaption = 'Edit';
 
-    if (isEditing) {
-        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />;
-        // let btnCaption = 'Save';
+	if (isEditing) {
+		editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />;
+		// let btnCaption = 'Save';
 
-    }
+	}
 
-    return (
-        <li>
-            <span className="player">
-                {editablePlayerName}
-                <span className="player-symbol">{symbol}</span>
-            </span>
-            <button onClick={handleEditClick}>{isEditing ? 'Save' : "Edit"}</button>
-        </li>
-    );
+	return (
+		<li>
+			<span className="player">
+				{editablePlayerName}
+				<span className="player-symbol">{symbol}</span>
+			</span>
+			<button onClick={handleEditClick}>{isEditing ? 'Save' : "Edit"}</button>
+		</li>
+	);
 }
